1495dccd3db8bcdc8fa800f1747c609b3281fd86
ghostingo@protonmail.ch <ssrakshith@gmail.com>
2021-12-24 16:17:03 +0530

    Added js for click events

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+let project1 = document.getElementById('project1');
+let project1Info = document.getElementById('project1-info');
+
+let project2 = document.getElementById('project2');
+let project2Info = document.getElementById('project2-info');
+
+project1.onclick = () => {
+	project1Info.style.display="block";
+}
+
+project2.onclick = () => {
+	project2Info.style.display = "block";
+}
